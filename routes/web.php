6f8429713b3d76cc3dc88577fe1c54014f79354c
VictorMoisesHonorCastro <73166294+VictorMoisesHonorCastro@users.noreply.github.com>
2023-02-23 19:49:16 +0100

    commit

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Livewire\CartComponent;
 use App\Http\Livewire\HomeComponent;
+use App\Http\Livewire\ShopComponent;
+use App\Http\Livewire\CheckoutComponent;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,3 +21,7 @@ use Illuminate\Support\Facades\Route;
     return view('welcome');
 });*/
 Route::get('/',HomeComponent::class)->name('home.index');
+Route::get('/shop',ShopComponent::class)->name('shop');
+Route::get('/cart',CartComponent::class)->name('shop.cart');
+Route::get('/checkout',CheckoutComponent::class)->name('shop.checkout');
+
